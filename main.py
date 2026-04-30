0af966c458409d0e82dc948b8b98fc3bceaecf60
binaryCoder-101 <syed.misak@gmail.com>
2026-04-30 17:22:07 +0530

    Loop logic and some refactors

--- main.py
+++ main.py
@@ -27,7 +27,15 @@ def main():
     if verbose_flag:
         print(f"User prompt: {prompt}\n")
     
-    generate_response(client, messages, verbose_flag)
+    for _ in range(20):
+        response = generate_response(client, messages, verbose_flag)
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+            messages.append(types.Content(role="user", parts=response))
+
+        
+                
 
 def generate_response(client, messages, verbose_flag):
     response = client.models.generate_content(
@@ -46,8 +54,10 @@ def generate_response(client, messages, verbose_flag):
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    print("Response:")
-    print(response.text)
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        return 
 
     results = []
     for function_call in response.function_calls:
@@ -62,7 +72,7 @@ def generate_response(client, messages, verbose_flag):
         if verbose_flag:
             print(f"-> {function_call_result.parts[0].function_response.response}")
         results.append(function_call_result.parts[0])
-
+    return response
 
 if __name__=="__main__":
     main()
